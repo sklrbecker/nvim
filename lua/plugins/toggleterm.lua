598c72af070429d9a011e3381c68de4659c96523
sklrbecker <41128548+sklrbecker@users.noreply.github.com>
2025-02-05 16:44:21 -0700

    add toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,8 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = true,
+	keys = {
+		{ "<leader>t", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle floating terminal" },
+	},
+}
