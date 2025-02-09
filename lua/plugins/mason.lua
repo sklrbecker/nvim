910d55fcd4c5bf3717d3bb46dd23c79a9a647a8f
sklrbecker <41128548+sklrbecker@users.noreply.github.com>
2025-02-09 11:53:08 -0700

    add gopls to auto-install

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -19,6 +19,7 @@ return {
         "prettier",
         "stylua", -- lua formatter
         "black", -- python formatter
+        "gopls", -- go language server
         "gofumpt", -- go formatter
       },
     })
